e223522d4cdcf58a9d56c98951d5928708c1a55a
amikhaielYoungDev <amikhaiel@protonmail.ch>
2017-02-28 20:30:19 +0100

    Initial Commit

---
+++ SHA1Converter-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SHA1Converter-iOS
+//
+//  Created by adi on 28.02.17.
+//  Copyright © 2017 adi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
